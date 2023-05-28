e8c22be61db5021acbea8ec60e0359f081013129
Iurii Potapov <samec2011@gmail.com>
2023-05-28 14:07:11 +0700

    logo

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -1,9 +1,11 @@
 import styles from './style.module.css'
 import { Container, LinkComponent } from '../index'
+import MyImage from './logo_footer.png';
 
 const Footer = () => {
   return <footer className={styles.footer}>
       <Container className={styles.footer__container}>
+        <img src={MyImage} alt="logo" />
         <LinkComponent href='#' title='На-изи' className={styles.footer__brand} />
       </Container>
   </footer>
